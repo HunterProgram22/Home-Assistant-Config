5beb45765c123a62d3372d2ee869c2b967cb21c0
Justin Kudela <JustinKudela@gmail.com>
2017-12-28 16:27:35 -0500

    Added python script for testing.

--- apps/yellow.py
+++ apps/yellow.py
@@ -5,3 +5,8 @@ class YellowAlert(appapi.AppDaemon):
     def initizalize(self):
         self.log("This is a Yellow Alert")
         self.log("All hands prepare for testing!")
+        self.set_lights()
+
+    def set_lights(self):
+        self.turn_on("light.tall_office_lamp")
+        self.turn_on("light.top_track_bulb")
