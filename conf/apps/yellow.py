fa8efca58dc7ed6854c95168b03615bc3eedde67
Justin Kudela <JustinKudela@gmail.com>
2017-12-30 02:43:25 +0000

    Updated test python script.

--- conf/apps/yellow.py
+++ conf/apps/yellow.py
@@ -3,14 +3,13 @@ import appdaemon.appapi as appapi
 class YellowAlert(appapi.AppDaemon):
 
     def initialize(self):
-        self.log("This is a Yellow Alert")
-        self.log("All hands prepare for testing!")
-        self.set_lights()
-        self.say_it_loud()
+        self.handle = self.listen_state(self.set_lights, "light.desk_lamp")
 
-    def set_lights(self):
+
+    def set_lights(self, **kwargs):
         self.turn_on("light.tall_office_lamp")
         self.turn_on("light.top_track_bulb", color_name="blue")
+        self.say_it_loud()
 
     def say_it_loud(self):
         self.call_service("tts/google_say",
